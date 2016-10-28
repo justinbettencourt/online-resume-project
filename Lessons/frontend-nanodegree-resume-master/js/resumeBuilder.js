91af81bef59c8fe0d6855208d6a53c7c9c88a6e7
Justin Bettencourt <justinvbettencourt@gmail.com>
2016-10-28 10:17:14 -0400

    * Project Reset

--- Lessons/frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ Lessons/frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -1,19 +1,3 @@
-var name = "Justin Bettencourt";
-
-var role = "Front-End Web Developer"
-
-var age = 24;
-
-var email = "justinvbettencourt@live.ca";
-
-var newEmail = email.replace("live", "gmail");
-
-var formattedName = HTMLheaderName.replace("%data%", name);
-
-var formattedRole = HTMLheaderRole.replace("%data%", role);
-
-var skills = ["Skill 1", "Skill 2", "Skill 3", "Skill 4"];
-
-$("#header").append(formattedName);
-$("#header").append(formattedRole);
-$("#main").append(skills.length);
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
