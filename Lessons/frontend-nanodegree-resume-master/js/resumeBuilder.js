fef4960798013e3ac98b2345e83d4212a18115ef
Justin Bettencourt <justinvbettencourt@gmail.com>
2016-11-04 08:57:02 -0400

    + Moved further along with the project

--- Lessons/frontend-nanodegree-resume-master/js/resumeBuilder.js
+++ Lessons/frontend-nanodegree-resume-master/js/resumeBuilder.js
@@ -1,3 +1,47 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name" : "Justin Bettencourt",
+    "role" : "Front-End Web Developer",
+    "contacts" : {
+        "email" : "justinvbettencourt@gmail.com",
+        "github" : "justinbettencourt",
+        "homeLocation" : "London, Ontario"
+    },
+    "welcomeMessage" : "This is my resume!",
+    "skills" : [
+        "HTML", "CSS", "JavaScript" 
+    ],
+    "biopic" : "images/fry.jpg"
+}
+
+var education = {
+    "schools" : [{
+        "name" : "George Brown College",
+        "location" : "Toronto, Ontario",
+        "degree" : "Advanced Diploma",
+        "major" : "Game Development",
+        "dates" : "2010 - 2013"
+    }],
+    "onlineCourses" : [{
+        "name" : "Udacity",
+        "degree" : "Nanodegree",
+        "major" : "Front-End Web Development"
+    }]
+};
+
+var work = {
+    "jobs" : [{
+        "employer" : "Yellow Bear Studios",
+        "position" : "3D Generalist & Game Developer",
+        "location" : "Toronto, Ontario",
+        "dates" : "August 2015 - August 2016",
+        "description" : "3D modeling and game development"
+    }]
+};
+ 
+var projects = {
+    "projects" : [{
+        "title" : "Love It or List It",
+        "dates" : "October 2015 - May 2016",
+        "description" : "Mobile Game"
+    }]
+}
